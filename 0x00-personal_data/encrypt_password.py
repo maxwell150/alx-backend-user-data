14e30a4bcd8d2039ef0c19bc28e603d45661eea9
max <maxwelgithumbi@gmail.com>
2022-05-12 15:25:47 +0300

    encrypt passwd

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""encrypting user passwd in db"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password, which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
