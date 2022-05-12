21887ae0efc375a78dc3f5fb333599315e68165c
max <maxwelgithumbi@gmail.com>
2022-05-12 14:41:37 +0300

    lof formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,30 @@
 """
 from typing import List
 import re
+import logging
 
 PII_FILEDS = ("name", "email", "phone", "ssn", "password")
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """constructor
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                  redaction: str, message: str,
