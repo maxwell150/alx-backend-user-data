d9ac9f1b077a43f1f2ea1d9623be96b901d49990
max <maxwelgithumbi@gmail.com>
2022-05-12 15:29:20 +0300

    check validity

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if password is valid"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
