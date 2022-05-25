104c140a928d33d9f52053d15c6acc36b160205c
max <maxwelgithumbi@gmail.com>
2022-05-25 20:33:08 +0300

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,6 +28,28 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST to create a new session"""
+    form = request.form
+    if "email" not in form:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form:
+          return jsonify({"message": "password required"}), 400
+    else:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if Auth.valid_login(email, password) is False:
+            abort(401)
+        else:
+            session_id = Auth.create_session(email)
+            response = jsonify({
+                "email": email,
+                "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
