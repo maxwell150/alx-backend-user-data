25f468a5524d984a0298804d0081ee04ea276030
max <maxwelgithumbi@gmail.com>
2022-05-25 21:24:15 +0300

    resete token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,6 +69,19 @@ def profile() -> str:
     else:
         abort(403)
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """respond to the POST /reset_password route."""
+    try:
+        email = request.form["email"]
+    except KeyError:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
